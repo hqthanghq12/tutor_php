ac4a0840499b95192486e0a13051b705b57b6aa7
trungnt173 <trungnt173@fpt.edu.vn>
2023-12-02 18:29:14 +0700

    first commit

--- add.php
+++ add.php
@@ -1,3 +1,34 @@
+<?php
+session_start();
+
+require_once 'connect_database.php';
+$listCT = "SELECT * FROM category";
+$result = $conn->query($listCT)->fetchAll();
+
+if(isset($_POST['btnSave'])){
+  
+        $product_name = $_POST['product_name'];
+        $category_id = $_POST['category_id'];
+        $price = $_POST['price'];
+        $quantity = $_POST['quantity'];
+        // thư mục sẽ được lưu ảnh vào thư mục image
+        $targetDir = "images/";
+        //Đường dẫn đến file được lưu
+        $targetFile = $targetDir.$_FILES['image']['name'];
+        
+
+        // Tiến hành upload file ảnh
+        if(move_uploaded_file($_FILES['image']['tmp_name'],$targetFile)){
+            $image_url = $targetFile;
+        }
+
+        $sql = "INSERT INTO products VALUES (NULL,'$product_name', '$price', '$image_url', '$quantity', '$category_id')";
+        $check =  $conn->exec($sql);
+        if($check){
+            header("location:index.php");
+        }
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,7 +37,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>Form thêm khóa học</title>
+    <title>Form thêm sản phẩm</title>
 </head>
 <body>
     <div class="add-form">
@@ -22,21 +53,24 @@
                 <label for="category">Danh mục:</label>
                 <select id="category" name="category_id">
                     <option value="">Chọn danh mục</option>
-                        <option value="">Danh Mục</option>
-                        <option value="">Danh Mục</option>
+                    <?php
+                    foreach ($result as $value ){
+                    ?>
+                        <option value="<?php echo $value['id'] ?>"><?php echo $value['category_name']?></option>
+                    <?php } ?>
                 </select>
                 <span></span>
             </div>
 
             <div class="form-group">
                 <label for="price">Giá:</label>
-                <input type="text" name="price" id="pricee" placeholder="Nhập giá">
+                <input type="text" name="price" id="price" placeholder="Nhập giá">
                 <span></span>
             </div>
 
             <div class="form-group">
                 <label for="image">Ảnh:</label>
-                <input type="file" name="image_url" accept="image/*">
+                <input type="file" name="image" accept="image/*">
                 <span></span>
             </div>
             <div class="form-group">
--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+require_once 'connect_database.php';
+
+//truy vấn cơ sở dữ liệu để lấy danh sách sản phẩm
+
+if(isset($_GET['btnSearch']) && !empty($_GET['txtsearch'])){
+    $sql = "SELECT pr.id, pr.product_name, pr.price, pr.image, pr.quantity, ca.category_name FROM products AS pr INNER JOIN category AS ca WHERE ca.id = pr.id AND pr.product_name LIKE '%".$_GET['txtsearch']."%';;";
+}else{
+    $sql = "SELECT pr.id, pr.product_name, pr.price, pr.image, pr.quantity, ca.category_name FROM products AS pr INNER JOIN category AS ca WHERE ca.id = pr.category_id;";
+}
+
+// Hứng toàn bộ dữ liệu từ database nhả xuống
+//$stmt = $conn->query($sql);
+//$result = $stmt ->fetchAll(PDO::FETCH_ASSOC);
+// hoặc
+
+$resut = $conn->query($sql)->fetchAll();
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -35,39 +55,21 @@
                 <th>Danh mục</th>
                 <th>Thao tác</th>
             </tr>
+            <?php
+            foreach ($resut as $value){
+            ?>
             <tr>
-                <td>OPPO A58</td>
-                <td><img src="images/image1.png" alt=""></td>
-                <td>4990000</td>
-                <td>50</td>
-                <td>Điện thoại</td>
+                <td><?php echo $value['product_name'] ?></td>
+                <td><img style="width: 150px; height: 150px" src="<?php echo isset($value['image'])? $value['image']:'' ?>"></td>
+                <td><?php echo $value['price'] ?></td>
+                <td><?php echo $value['quantity'] ?></td>
+                <td><?php echo $value['category_name'] ?></td>
                 <td>
-                    <a class="edit-btn" href="#">Sửa</a>
-                    <a class="delete-btn" href="#">Xóa</a>
+                    <a class="edit-btn" href=">">Sửa</a>
+                    <a class="delete-btn" href="">Xóa</a>
                 </td>
             </tr>
-            <tr>
-                <td>iPhone 15 Pro Max</td>
-                <td><img src="images/image2.png" alt=""></td>
-                <td>33990000</td>
-                <td>50</td>
-                <td>Điện thoại</td>
-                <td>
-                    <a class="edit-btn" href="#">Sửa</a>
-                    <a class="delete-btn" href="#">Xóa</a>
-                </td>
-            </tr>
-            <tr>
-                <td>MacBook Pro 16</td>
-                <td><img src="images/image4.png" alt=""></td>
-                <td>64990000</td>
-                <td>50</td>
-                <td>Laptop</td>
-                <td>
-                    <a class="edit-btn" href="#">Sửa</a>
-                    <a class="delete-btn" href="#">Xóa</a>
-                </td>
-            </tr>
+            <?php } ?>
         </table>
     </div>
 </body>
